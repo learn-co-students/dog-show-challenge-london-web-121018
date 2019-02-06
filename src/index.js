18e524c31bca1629b746d01113956fb3381ca9f6
github username <github email address>
2019-02-06 10:57:44 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@
 
 const table = document.querySelector('table');
 const tableBody = document.querySelector('#table-body')
+const inputName = document.querySelector('#inputName')
+const inputBreed = document.querySelector('#inputBreed')
+const inputSex = document.querySelector('#inputSex')
+
 
 const state = {
-  dogs: []
+  dogs: [],
+  selectedDog: null
 }
 
 // add a single dog to the page
@@ -20,7 +25,7 @@ function renderSingleDog(dog) {
     <td class="padding">${dog.name}</td>
     <td class="padding">${dog.breed}</td>
     <td class="padding">${dog.sex}</td>
-    <td class="padding center"><button>Edit</button></td>
+    <td class="padding center edit-btn"><button>Edit Dog</button></td>
   `;
   tableBody.appendChild(tableRow);
 }
@@ -31,6 +36,22 @@ function renderMultipleDogs(dogs) {
   dogs.forEach(dog => renderSingleDog(dog))
 }
 
+// event listener for edit button
+
+// function clickToEdit() {
+  document.addEventListener('click', event => {
+    if (event.target.className === 'edit-btn') {
+      const id = parseInt(event.target.dataset.id)
+      const foundDog = state.dogs.find(dog => dog.id === id)
+      // inputName.placeholder = dog.name
+      // inputBreed.placeholder = dog.breed
+      // inputSex.placeholder = dog.sex
+      updateDog(foundDog)
+    }
+  })
+// }
+
+// initializer
 function initialize() {
   getDogs()
     .then(dogs => {
@@ -46,5 +67,15 @@ function getDogs() {
   return fetch('http://localhost:3000/dogs')
       .then(resp => resp.json())
 }
+
+function updateDog() {
+  const dog = state.selectedDog
+  return fetch(`http://localhost:3000/dogs/${dog.id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dog)
+  }).then(resp => resp.json())
+}
+
 
 initialize()
